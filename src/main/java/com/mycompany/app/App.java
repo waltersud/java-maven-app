51c575eee3464ba0e24a4491b647a47915de9c9a
Walter Arevalo <waltersud@gmail.com>
2024-04-23 11:36:17 -0500

    Segundo cambios

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello Word , haciendo el cambio numero 1";
 
     public App() {}
 
